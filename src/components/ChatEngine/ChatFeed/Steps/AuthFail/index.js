74bc51ef2a4dd97d195e41f6cc22d00d0cbe4f87
Maxime Ferrino <maxime@crowdaa.com>
2023-06-02 16:56:29 +0400

    Fixed an issue with react fragments. Bumped react to v18

--- src/components/ChatEngine/ChatFeed/Steps/AuthFail/index.js
+++ src/components/ChatEngine/ChatFeed/Steps/AuthFail/index.js
@@ -7,32 +7,26 @@ const AuthFail = (props) => {
     const project = props.projectID ? props.projectID : props.publicKey
     return (
         <div
-            id='ce-login-fail-breaker' 
+            id='ce-login-fail-breaker'
             style={{ width: '100%', textAlign: 'center', paddingTop: 'calc(50% - 112px)' }}
         >
-            <div 
-                id='ce-login-fail-text' 
+            <div
+                id='ce-login-fail-text'
                 style={{ color: '#afafaf', fontWeight: '600', fontSize: '14px', marginBottom: '6px', marginRight: '32px', marginLeft: '32px' }}
             >
                 {translate(
                     'Your credentials are incorrect. Make sure your Project ID, Username, and Password are correct here',
-                    'AuthFail',
-                    (
-                        <>
-                            Your credentials are incorrect. Make sure your Project ID, Username, and Password are correct
-                            <a href={`https://chatengine.io/projects/${project}`}>here</a>
-                        </>
-                    )
+                    'AuthFail'
                 )}
             </div>
 
             <img
-                id='ce-login-fail-gif' 
+                id='ce-login-fail-gif'
                 style={{ width: '50%', maxWidth: '200px' }}
                 src='https://chat-engine-assets.s3.amazonaws.com/welcome_gifs/no.gif'
                 alt='chat-engine-login-fail'
             />
-        </div>   
+        </div>
     )
 }
 
